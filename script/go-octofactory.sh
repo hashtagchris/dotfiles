c3bf7eac8f38b3f28fe502699f4cf0933ff00858
Chris Sidi <hashtagchris@github.com>
2021-12-11 02:43:47 +0000

    More prompts

--- script/go-octofactory.sh
+++ script/go-octofactory.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# Usage: . ./go-octofactory.sh && go get -u github.com/github/monolith-twirp-actions/core@v1.0.99 && go mod vendor
+# Usage: . ./go-octofactory.sh && dev-vpn connect && go get -u github.com/github/monolith-twirp-actions/core@v1.0.99 && go mod vendor
 
-read -p "Join the dev vpn and hit enter: " VPN
-read -p "Go to https://octofactory.githubapp.com/artifactory/webapp/#/profile in your browser" BROWSER
-read -p "API token: " OCTOFACTORY_API_TOKEN
+read -p "Did you remember to prefix your execution with \". \" so the exports would stick?" BLAH
+read -p "Step 1. Join the dev-vpn on your machine" VPN
+read -p "Step 2. Go to https://octofactory.githubapp.com/artifactory/webapp/#/profile in your browser, hit enter" BROWSER
+read -p "Step 3. Copy API token: " OCTOFACTORY_API_TOKEN
 
 # https://github.com/github/go-lang/blob/main/docs/octofactory.md#configure-your-go-environment
 export GOPROXYUSER=hashtagchris
@@ -13,3 +14,5 @@ export GOPRIVATE='*github.com/github/*'
 
 # https://github.com/github/monolith-twirp/blob/master/docs/usage.md#go
 export GONOPROXY='none'
+
+echo "Go environment variables set"
